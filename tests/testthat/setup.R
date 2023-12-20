c23f1b0b0bb0c518418521a252dd83b10f2647c2
Eliot McIntire <eliotmcintire@gmail.com>
2023-12-20 12:35:11 -0800

    tweak to new approach to OMP_THREADS_LIMIT

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,7 @@
 library(data.table)
 origDTthreads <- getDTthreads()
-setDTthreads() # this triggers data.table to read the OMP_THREAD_LIMIT
 if (!identical(Sys.getenv("NOT_CRAN"), "true")) { # this asks "is this test running on CRAN"
   Sys.setenv("OMP_THREAD_LIMIT" = 1)
-  setDTthreads() # this triggers data.table to read the OMP_THREAD_LIMIT
   withr::defer(
     {
       data.table::setDTthreads(origDTthreads)
@@ -11,3 +9,4 @@ if (!identical(Sys.getenv("NOT_CRAN"), "true")) { # this asks "is this test runn
     teardown_env()
   )
 }
+setDTthreads() # this triggers data.table to read the OMP_THREAD_LIMIT
